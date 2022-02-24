568ed4a45280b0781b618cf1b042badeb2255488
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-02-24 11:41:08 -0700

    monitoring demo

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'../public/index.html'))
+})
+
+app.use(express.static(path.join(__dirname,'../public')));
+
+app.listen(port, () => console.log(`Port running on ${port}`));
